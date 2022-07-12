19b10523b9846370c989d55b4046b888f59314e2
Nikhil <nikhil.shah2008@gmail.com>
2022-07-12 13:16:25 +0530

    Day 2

---
+++ day 2/shoppingAPPMVC/shoppingAPPMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPPMVC.Models;
+using System.Diagnostics;
+
+namespace shoppingAPPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //collect data from Model here
+            //Model is a cs file, create a new new object of model class and call the method
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ day 2/shoppingAPPMVC/shoppingAPPMVC/Controllers/ShoppingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shoppingAPPMVC.Controllers
+{
+    public class ShoppingController : Controller
+    {
+      public IActionResult Productlist()
+        {
+            //this data will usually come from model, (model decides where it comes from)
+            List<string> pList = new List<string>()
+            {
+                "Cold-Drinks",
+                "Electronics",
+                "Cloths",
+                "Shoes",
+                "Accessories",
+                "Eateries"
+            };
+
+            ViewBag.products = pList;
+            ViewBag.totalProducts = pList.Count;
+            return View();
+        }
+
+        public IActionResult SearchProduct()
+        {
+            return View();
+        }
+
+        public IActionResult productSale()
+        {
+            return View();
+        }
+    }
+}
