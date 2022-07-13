d4556f79c612cc93d2daee1445902d3d348e264a
Nikhil <nikhil.shah2008@gmail.com>
2022-07-13 13:18:41 +0530

    Day 3

---
+++ day 3/shoppingAPP/shoppingAPP/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shoppingAPP.Models;
+namespace shoppingAPP.Controllers
+{
+    public class ProductsController : Controller
+    {
+        Products pObj = new Products();
+
+        public IActionResult DisplayProducts()
+        {
+            ViewBag.products = pObj.GetAllProducts();
+            return View();
+        }
+                
+        public IActionResult SearchProduct()
+        {
+            ViewBag.hasData = false;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchProduct(int productId)
+        {   
+            ViewBag.hasData = true;
+            ViewBag.productinfo = pObj.GetProductById(productId);
+            return View();
+        }
+
+
+
+    }
+}
---
+++ day 3/shoppingAPP/shoppingAPP/Models/Products.cs
@@ -0,0 +1,71 @@
+﻿namespace shoppingAPP.Models
+{
+    public class Products
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+
+        static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId = 101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new Products(){ pId = 102, pName="Coke", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new Products(){ pId = 103, pName="Maggie", pCategory="Fast-Food", pIsInStock=false, pPrice=50},
+            new Products(){ pId = 104, pName="IPhone", pCategory="Electronics", pIsInStock=true, pPrice=50},
+            new Products(){ pId = 105, pName="2 States", pCategory="Book", pIsInStock=true, pPrice=50},
+            new Products(){ pId = 106, pName="Sandwitch", pCategory="Fast-Food", pIsInStock=false, pPrice=50}
+        };
+
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public Products GetProductById(int id)
+        {
+            var p = (from pr in pList
+                     where pr.pId == id
+                     select pr).Single();         
+
+            if (p != null)
+            {
+                return p;
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public string AddProduct(Products newProduct)
+        {
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string DeleteProduct(int id)
+        {
+            var p = (from pr in pList
+                     where pr.pId == id
+                     select pr).Single();
+
+            pList.Remove(p);
+            return "Product Removed Successfully";
+        }
+
+        public string UpdateProduct(Products changes)
+        {
+            var p = (from pr in pList
+                     where pr.pId == changes.pId
+                     select pr).Single();
+
+            p.pName = changes.pName;
+            p.pCategory = changes.pCategory;
+            p.pPrice = changes.pPrice;
+            p.pIsInStock = changes.pIsInStock;
+
+            return "Product Updated Successfully";
+        }
+
+    }
+}
